60df431554bcb2e6a503a5dd09a7a17102760309
marianphillips <79763160+marianphillips@users.noreply.github.com>
2022-05-23 17:00:06 +0100

    added search steps

--- src/controllers/wine.js
+++ src/controllers/wine.js
@@ -1,23 +1,68 @@
-import { sendDataResponse, sendMessageResponse } from '../utils/responses.js'
-import { findWineById, findManyWines } from '../domain/wine.js'
+import { sendDataResponse, sendMessageResponse } from "../utils/responses.js";
+import {
+  findWineById,
+  fullSearch,
+  withVarietalsSearch,
+  middleSearch,
+  limitedSearch,
+  basicSearch,
+  semiBasicSearch,
+} from "../domain/wine.js";
 
 export const findWine = async (req, res) => {
+  //get list of wines matching criteria, reducing criteria as you go until there is a match
 
-    try {  
-       const search = await findManyWines(req)
-    
-      if (!search) {
-        return sendMessageResponse(res, 400, 'no match')
-      }  
+  try {
+    let search = await fullSearch(req);
 
-      const wine = search[2]
-
-      if (!wine) {
-        return sendMessageResponse(res, 400, 'wine does not exist in db')
-      }  
-
-      return sendDataResponse(res, 201, wine)
-    } catch (error) {
-      return sendMessageResponse(res, 500, 'Server Error')
+    if (search.length === 0) {
+      search = await withVarietalsSearch(req);
     }
+
+    if (search.length === 0) {
+      search = await middleSearch(req);
+    }
+
+    if (search.length === 0) {
+      search = await limitedSearch(req);
+    }
+
+    if (search.length === 0) {
+      search = await semiBasicSearch(req);
+    }
+
+    if (search.length === 0) {
+      search = await basicSearch(req);
+    }
+
+      //filter of results again through characteristics
+    const filteredSearch = search.filter(wine => {
+      for(let character of req.body.characteristics) {
+        if(wine.characteristics.includes(character)) {
+          return true
+        }
+      }
+      return false
+    })
+
+    if(filteredSearch.length !== 0) {
+      search = filteredSearch
+    }
+
+      //check for particular ids and set wine to push certain brands
+
+      const maxRandom = search.length;
+
+      const wine = search[Math.floor(Math.random() * maxRandom)];
+      console.log(wine)
+      if (!wine) {
+        return sendMessageResponse(res, 400, "wine does not exist in db");
+      }
+
+      return sendDataResponse(res, 201, wine);
+    }
+   catch (error) {
+     console.log(error)
+    return sendMessageResponse(res, 500, "Server Error");
   }
+};
--- src/domain/wine.js
+++ src/domain/wine.js
@@ -1,5 +1,15 @@
 import dbClient from '../utils/dbClient.js'
 
+function generalQuery(req, query) {
+    if(req.body.type !== "na" && req.body.type) query.type = req.body.type
+    if(req.body.sweet !== "na" && req.body.sweet) query.sweet = req.body.sweet
+    if(req.body.sweet === "off dry also") query.sweet = "off dry"
+    if(req.body.body !== "na" && req.body.body) query.body = req.body.body
+    if(req.body.traits.includes("vegan") || req.body.traits.includes("veg")) query.vegan = "Y"
+    if(req.body.traits.includes("natural")) query.natural = "Y"
+    if(req.body.traits.includes("sustainable")) query.sustainable = "Y"
+}
+
 export async function findWineById(id) {
     const wine = await dbClient.wine.findUnique({
         where : {
@@ -13,11 +23,141 @@ export async function findWineById(id) {
     return wine
   }
 
-  export async function findManyWines(req) {
+  export async function fullSearch(req) {
+    try{
     const query = {}
 
-    if(req.body.type) query.type = req.body.type
+    generalQuery(req, query)
 
+    let orQuery = []
+
+    if(req.body.varietals.length !== 0) {
+        for(let v of req.body.varietals) {
+            if(v) {
+                orQuery.push({
+                    varietal: {
+                        contains: v
+                    }
+                })
+            }
+        }
+    }
+
+    if(req.body.characteristics.length !== 0) {
+        for(let character of req.body.characteristics) {
+            if(character) {
+                orQuery.push({
+                    characteristics: {
+                        contains: character
+                    }
+                })
+            }
+        }
+    }
+
+    
+    query.OR = orQuery
+
+    const wines = await dbClient.wine.findMany({
+        where : query
+        })
+
+    return wines
+    } catch (error) {
+        return []
+    }
+  }
+
+  export async function withVarietalsSearch(req) {
+    try{
+        const query = {}
+    
+        generalQuery(req, query)
+    
+        let varietalQuery = []
+    
+        if(req.body.varietals.length !== 0) {
+            for(let v of req.body.varietals) {
+                if(v) {
+                    varietalQuery.push({
+                        varietal: {
+                            contains: v
+                        }
+                    })
+                }
+            }
+        }
+    
+        query.OR = varietalQuery
+    
+        const wines = await dbClient.wine.findMany({
+            where : query
+            })
+    
+        return wines
+        } catch (error) {
+            return []
+        }
+      }
+  
+
+  export async function middleSearch(req) {
+    try {
+    const query = {}
+
+    generalQuery(req, query)
+
+    const wines = await dbClient.wine.findMany({
+        where : query
+        })
+
+    return wines
+    } catch (error) {
+        return []
+    }
+  }
+
+  export async function limitedSearch(req) {
+      try {
+    const query = {}
+
+    if(req.body.type !== "na" && req.body.type) query.type = req.body.type
+    if(req.body.sweet !== "na" && req.body.sweet) query.sweet = req.body.sweet
+    if(req.body.sweet === "off dry also") query.sweet = "off dry"
+    if(req.body.body !== "na" && req.body.body) query.body = req.body.body
+    if(req.body.traits.includes("vegan") || req.body.traits.includes("veg")) query.vegan = "Y"
+
+    const wines = await dbClient.wine.findMany({
+        where : query
+        })
+
+    return wines
+    } catch (error) {
+        return []
+    }
+  }
+
+  export async function semiBasicSearch(req) {
+    try {
+    const query = {}
+
+    if(req.body.type !== "na" && req.body.type) query.type = req.body.type
+    if(req.body.sweet !== "na" && req.body.sweet) query.sweet = req.body.sweet
+
+    const wines = await dbClient.wine.findMany({
+        where : query
+        })
+
+    return wines
+    } catch (error) {
+        return []
+    }
+  }
+
+  export async function basicSearch(req) {
+    const query = {}
+
+    if(req.body.type !== "na" && req.body.type) query.type = req.body.type
 
     const wines = await dbClient.wine.findMany({
         where : query
